8250ca8b843e11fd28d30514ebdcc96fcea21cec
praful-parmar <41146328+praful-parmar@users.noreply.github.com>
2021-11-10 13:32:18 +0530

    second
    second

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.debug = True
 //tdfgddgdfggd
 //tgdfgghdfhfhfhghghdhdddgdgdfinal
 //first
+//second
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
